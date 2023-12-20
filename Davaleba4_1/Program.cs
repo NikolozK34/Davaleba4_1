2616f9986aea827a48a09e2b2f3d6c4a02fcb229
NikolozK34 <kekelidze97@gmail.com>
2023-12-20 13:25:20 +0400

    Add project files.

---
+++ Davaleba4_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int[,] matrix = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i, j] = rand.Next(-100, 101);
+            }
+        }
+      
+        Console.WriteLine("matrica:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"{matrix[i, j],4} ");
+            }
+            Console.WriteLine();
+        }
+
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int currentNumber = matrix[i, j];
+                min = Math.Min(min, currentNumber);
+                max = Math.Max(max, currentNumber);
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int currentNumber = matrix[i, j];
+
+                if (currentNumber >= min && currentNumber <= max)
+                {
+                    sum += currentNumber;
+                }
+            }
+        }
+
+        Console.WriteLine($"jami aris: {sum}");
+    }
+
+}
